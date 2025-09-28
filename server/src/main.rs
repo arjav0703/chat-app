f20e0c66f1a3435fbea88751933efb08a3d82b0a
Arjav Jain <arjavjain0703@gmail.com>
2025-09-28 12:27:59 +0530

    accept connection [server]

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::Local;
+use serde::{Deserialize, Serialize};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+    sync::broadcast,
+};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Message {
+    username: String,
+    content: String,
+    timestamp: String,
+    message_type: MessageType,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+enum MessageType {
+    Broadcast,
+    UserMessage,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting server...");
+
+    let bind_addr = "127.0.0.1:6969";
+
+    let listener = TcpListener::bind(bind_addr).await?;
+
+    let (_tx, _rx) = broadcast::channel::<String>(100);
+
+    loop {
+        let (_socket, addr) = listener.accept().await?;
+
+        println!(
+            "[{}] New connection from: {}",
+            Local::now().format("%H:%M:%S"),
+            addr
+        );
+    }
+
+    // Ok(())
 }
